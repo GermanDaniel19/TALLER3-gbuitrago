34b360b2ca9a0e96081bfc43bb43e732b94ff75a
German Daniel Buitrago <germandanielit@gmail.com>
2024-11-12 15:26:50 -0500

    primera version taller 2 modulo 2

---
+++ consola.py
@@ -0,0 +1,29 @@
+from huron import Huron
+from boa_Constrictor import Boa_Constrictor
+from guarderia import Guarderia
+
+
+
+"""
+Ejemplo
+"""
+
+# print("======huron=======")
+# huron1 = Huron(5.2,"guinea ecuatorial",115.8)
+# print(huron1.data_animal)
+# print("flete: " + str(huron1.calcular_flete()))
+# print(huron1.hacer_ruido())
+
+# print("======boa constrictor=======")
+# boa = Boa_Constrictor(15.4,"Amazonas",20.4)
+# print(boa.data_animal)
+# print("flete: " + str(boa.calcular_flete()))
+# print(boa.hacer_ruido())
+# print("ratones merendiados: " + str(boa.ratones_comidos()))
+# boa.comer_raton()
+# print("ratones merendiados: " + str(boa.ratones_comidos()))
+
+# Guarderia
+
+guarderia1 = Guarderia()
+guarderia1.alimentar_boa("boa1")
---
+++ huron.py
@@ -0,0 +1,11 @@
+from animal_exotico import Animal_Exotico
+
+class Huron(Animal_Exotico):
+    
+    def __init__(self, peso: float, pais_origen: str, impuestos: float) -> None:
+        super().__init__(peso, pais_origen, impuestos)
+
+    def hacer_ruido(self) -> str:
+        return "¡Eek Eek!"
+    
+
